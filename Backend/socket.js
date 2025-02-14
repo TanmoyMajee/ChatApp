4e82bc36ce95b50001aa590dce6f235911370118
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-02-14 16:57:50 +0530

    fix msg send recv

--- Backend/socket.js
+++ Backend/socket.js
@@ -12,16 +12,34 @@ const io = new Server(server, {
   }
 });
 
+const userIdMap = {};
+
 io.on('connection',(socket)=>{
   console.log(`new user is connected socketTD : ${socket.id}`)
+
+  const userId = socket.handshake.query.userId; // get userId from query
+  if(userId) userIdMap[userId] = socket.id; // store userId and socket.id in map
+
+  io.emit("getOnlineUsers",Object.keys(userIdMap)); // send all connected users to all clients
+  // // as the map key is userId, we can get all connected users by Object.keys(userIdMap)
+
+  socket.on("disconnect",()=>{
+      console.log(`user is disconnected socketID : ${socket.id}`)
+      const userId = Object.keys(userIdMap).find(key => userIdMap[key] === socket.id);
+      delete userIdMap[userId]; // remove disconnected user from map
+      io.emit("getOnlineUsers",Object.keys(userIdMap)); // send all connected users to all clients
+  })
 
   socket.on("join_room",(room)=>{
       socket.join(room);
       console.log("user join room :",room)
   })
 
-  socket.on("new_message",(message)=>{
-      let chat = message.chat;
+  socket.on("new_message",(data)=>{
+      // let chat = message.chat;
+       console.log("Received message:", data);
+    // Broadcast the message to everyone in the room (including the sender, if needed)
+    io.to(data.room).emit("message_received", data);
   })
 })
 
