a05f1698365c46a8724fecf7dfe500e38229a7e9
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-03-10 22:44:46 +0530

    .

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -27,5 +27,5 @@ userSchema.methods.matchPassword = async function (enteredPass){
   return await bcrypt.compare(enteredPass, this.password);
 }
 
-const UserModel = mongoose.model('User', userSchema);
+const UserModel = mongoose.model('ChatUser', userSchema);
 module.exports = UserModel;
