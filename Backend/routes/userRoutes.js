9c2ac08ec4109836a04297c1bb6473abdd005aff
TanmoyMjaee <tanmoymajee239@gmail.com>
2025-06-14 01:28:53 +0530

    add google login/register route

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {registerFun,authuserFun,allusersFun} = require('../controller/userController');
+const {registerFun,authuserFun,allusersFun,googleLogin,googleRegister} = require('../controller/userController');
 const protect = require('../middleware/authMiddleware');
 
 // router.route('/login').get(loginFun);
 router.post('/', registerFun)
 router.get('/', protect,allusersFun);
 router.post('/login', authuserFun);
+router.post('/google-login',googleLogin);
+router.post('/google-register',googleRegister);
 // both of these functions are in the userController.js file
 
 module.exports = router;
